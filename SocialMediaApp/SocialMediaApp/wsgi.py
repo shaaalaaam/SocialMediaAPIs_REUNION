745a860185e7f531f44b14a805683dbdae0a72f2
“shaaalaaam” <shaaalaaam@gmail.com>
2023-03-17 20:01:50 +0530

    After Creating Django project ( Corrected name)

--- SocialMediaApp/SocialMediaApp/wsgi.py
+++ SocialMediaApp/SocialMediaApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for APIapp project.
+WSGI config for SocialMediaApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APIapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocialMediaApp.settings')
 
 application = get_wsgi_application()
